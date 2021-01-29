c21fd934cf0a888de5683eeaa2e28655295a9d45
JvPelai <jv.pelai@gmail.com>
2021-01-29 11:04:55 -0300

    setup

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import Table from "./Table";
+
+class App extends Component { 
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      }
+    ],
+  }
+  render(){
+    const characters = [
+      
+    ]
+
+    return(
+      <div className="container">
+        <Table characterData={characters} />
+      </div>
+    )
+  }
+}
+
+export default App
